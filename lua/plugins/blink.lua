d6f93b89a34222c96aeb9bff5e2c29f6b2b7b02a
hacxy <hacxy.js@outlook.com>
2025-04-21 19:20:04 +0800

    chore: change snippets

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -18,6 +18,13 @@ return {
               score_offset = -1,
             },
           },
+
+          -- transform_items = function(_, items)
+          --   return vim.tbl_filter(
+          --     function(item) return item.kind ~= require("blink.cmp.types").CompletionItemKind.Snippet end,
+          --     items
+          --   )
+          -- end,
         },
         keymap = {
           ["<Tab>"] = { "snippet_forward", "fallback" },
