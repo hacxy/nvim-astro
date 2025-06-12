61d00685cf8fceef09449b9775996ad9767e2bd0
hacxy <hacxy.js@outlook.com>
2025-06-12 17:27:54 +0800

    chore: change config

--- lua/community.lua
+++ lua/community.lua
@@ -3,7 +3,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.typescript" },
-  -- { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.markdown" },
 
   -- colorscheme
